e8c804d23dd2fe4439d06bf5bb6f6208d8f9fa91
idefo <dekleidung@gmail.com>
2018-10-11 18:43:16 +0300

    Learning about DOM selectors for single elements

--- dom-manipulation.js
+++ dom-manipulation.js
@@ -36,3 +36,41 @@ scriptsArr.forEach(function(script) {
 });
 
 console.log(val);
+
+// document.getElementById()
+
+console.log(document.getElementById('task-title'));
+
+//Get things from the element
+console.log(document.getElementById('task-title').id);
+console.log(document.getElementById('task-title').className);
+
+const taskTitle = document.getElementById('task-title');
+
+// Change styling
+
+taskTitle.style.background = '#000';
+taskTitle.style.color = '#fff';
+taskTitle.style.padding = '5px';
+taskTitle.style.width = '150px';
+taskTitle.style.textAlign = 'center';
+
+// Change content
+taskTitle.textContent = 'Task List';
+taskTitle.innerHTML = 'My tasks';
+taskTitle.innerHTML = '<span style="color:red">Task List</span>';
+
+//document.querySelector()
+
+console.log(document.querySelector('#task-title'));
+console.log(document.querySelector('.task-class'))
+console.log(document.querySelector('h5'));
+
+document.querySelector('li').style.color = 'red';
+document.querySelector('ul li').style.color = 'blue';
+
+document.querySelector('li:last-child').style.color = 'red';
+document.querySelector('li:nth-child(3)').style.color = 'yellow';
+document.querySelector('li:nth-child(4)').textContent = 'Hello world';
+document.querySelector('li:nth-child(odd)').style.background = '#ccc';
+document.querySelector('li:nth-child(even)').style.background = '#ccc';
