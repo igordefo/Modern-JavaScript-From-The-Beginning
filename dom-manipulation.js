1d2cf593539974858d7f8cefc7ed2ae06cab2631
idefo <dekleidung@gmail.com>
2018-10-13 10:42:45 +0300

    Learning about traversing the DOM

--- dom-manipulation.js
+++ dom-manipulation.js
@@ -76,7 +76,7 @@ document.querySelector('li:nth-child(odd)').style.background = '#ccc';
 document.querySelector('li:nth-child(even)').style.background = '#ccc';
 
 // document.getElementsByClass
- const items = document.getElementsByClassName('collection-item');
+const items = document.getElementsByClassName('collection-item');
 
  console.log(items);
  console.log(items[0]);
@@ -95,10 +95,83 @@ lis[3].textContent = 'Another task';
 
 // Convert HTML collection into array
 lis = Array.from(lis);
-lis.reverse();
 
 lis.forEach(function(li, index){
   console.log(li);
   li.textContent = `${index}: Hello`;
 })
 console.log(lis);
+
+// document.querySelectorAll
+
+const items = document.querySelectorAll('ul.collection li.collection-item');
+
+items.forEach(function(item, index){
+  item.textContent = `${index}: Hello`;
+});
+
+const liOdd = document.querySelectorAll('li:nth-child(odd)');
+const liEven = document.querySelectorAll('li:nth-child(even)');
+
+liOdd.forEach(function(item, index){
+  item.style.background = '#ccc';
+});
+
+for(let i = 0; i < liEven.length; i++){
+  liEven[i].style.background = '#f5f5f9';
+};
+
+console.log(items);
+
+// Traversing DOM
+let trav;
+const list = document.querySelector('ul.collection');
+const listItem = document.querySelector('li.collection-item:first-child');
+
+trav = listItem;
+trav = list;
+
+// Get child nodes
+trav = list.childNodes;
+trav = list.childNodes[0];
+trav = list.childNodes[0].nodeName;
+trav = list.childNodes[3].nodeType;
+// 1 - Element
+// 2 - Attribute
+// 3 - Text node
+// 8 - Comment
+// 9 - Document itself
+// 10 - Doctype
+
+// Get children nodes
+trav = list.children;
+trav = list.children[1];
+list.children[1].textContent = 'Hello';
+// Children of children
+trav = list.children[3].children[0];
+list.children[3].children[0].id = 'test-link';
+// First child
+trav = list.firstChild;
+trav = list.firstElementChild;
+// Last child
+trav = list.lastChild;
+trav = list.lastElementChild;
+
+// Count child element
+trav = list.childElementCount;
+
+//Get parent node
+trav = listItem.parentNode;
+trav = listItem.parentElement;
+trav = listItem.parentElement.parentElement;
+
+// Get next sibling
+trav = listItem.nextSibling;
+trav = listItem.nextElementSibling;
+
+// Get prev sibling
+trav = listItem.previousSibling;
+trav = listItem.previousElementSibling;
+
+
+console.log(trav);
