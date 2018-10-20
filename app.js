1bed0c1e9ac09a43f0da99f3575721c8b60b7921
idefo <dekleidung@gmail.com>
2018-10-20 15:51:12 +0300

    Learning about replacement and event listners

--- app.js
+++ app.js
@@ -20,3 +20,73 @@ li.appendChild(link);
 document.querySelector('ul.collection').appendChild(li);
 
 console.log(li);
+
+// Replace element
+// Create Element
+const newHeading = document.createElement('h2');
+// Add id
+newHeading.id = 'task-title';
+// New text node
+newHeading.appendChild(document.createTextNode('World List'));
+// Get old heading
+const oldHeading = document.getElementById('task-title');
+// Parent
+const cardAction = document.querySelector('.card-action');
+//Replace
+cardAction.replaceChild(newHeading, oldHeading);
+// Remove element
+const lis = document.querySelectorAll('li');
+const list = document.querySelector('ul');
+// Remove list item
+lis[0].remove();
+// Remove child element
+list.removeChild(lis[3]);
+
+// Classes & attr
+const firstLi = document.querySelector('li:first-child');
+const linkLi = firstLi.children[0];
+
+let val;
+
+val = link.className;
+val = link.classList;
+val = link.classList[0];
+linkLi.classList.add('test');
+linkLi.classList.remove('test');
+val = linkLi;
+
+val = linkLi.getAttribute('href');
+val = linkLi.setAttribute('href', 'http://google.com');
+linkLi.setAttribute('title', 'Google');
+val = linkLi.hasAttribute('title');
+linkLi.removeAttribute('title');
+val = linkLi;
+
+console.log(val);
+
+// Event listners
+document.querySelector('.clear-tasks').addEventListener('click', onClick);
+
+function onClick (e) {
+  //console.log('Clicked');
+  let val;
+  val = e;
+  // Event target element
+  val = e.target;
+  val = e.target.id;
+  val = e.target.className;
+  val = e.target.classList;
+
+  // Event type
+  val = e.type;
+  // Timestamp
+  val = e.timeStamp
+  // Coords event relative to the windows
+  val = e.clientY;
+  val = e.clientX;
+  // Coords event relative to the element
+  val = e.offsetY;
+  val = e.offsetX;
+  
+  console.log(val);
+}
