18684f47db65560aeab1aef16a5a6fa24bf9e5a3
idefo <dekleidung@gmail.com>
2018-10-22 20:26:15 +0300

    Learning about mouse events

--- app.js
+++ app.js
@@ -90,3 +90,33 @@ function onClick (e) {
   
   console.log(val);
 }
+
+// Mouse events
+const clearBtn = document.querySelector('.clear-tasks');
+const card = document.querySelector('.card');
+const heading = document.querySelector('h5');
+
+// Click
+clearBtn.addEventListener('click', runEvent);
+// Double click
+clearBtn.addEventListener('dblclick', runEvent);
+// Mouse down
+clearBtn.addEventListener('mousedown', runEvent);
+// Mouse up
+clearBtn.addEventListener('mouseup', runEvent);
+// Mouse enter
+clearBtn.addEventListener('mouseenter', runEvent);
+// Mouse leave
+clearBtn.addEventListener('mouseleave', runEvent);
+// Mouse ovet
+card.addEventListener('mouseover', runEvent);
+// Mouse out
+card.addEventListener('mouseout', runEvent);
+// Mouse move
+card.addEventListener('mousemove', runEvent);
+// Event Handler
+function runEvent(e) {
+  console.log(`Event type: ${e.type}`); // Event type: click 
+  heading.textContent= `MouseX: ${e.offsetX} MouseY: ${e.offsetY}`;
+  document.body.style.backgroundColor = `rgb(${e.offsetX}, ${e.offsetY}, 40)`;
+}
